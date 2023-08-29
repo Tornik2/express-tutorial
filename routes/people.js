e4c5043db7ab11a4f0a5698500219fa081067b34
Tornik2 <tornik2@yahoo.com>
2023-08-29 19:38:05 +0400

    router practice

---
+++ routes/people.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const { getPeople, createPerson, getSinglePerson } = require('./router-utils')
+
+const router = express.Router()
+
+
+router.post('/', (req, res) => {
+  
+})
+
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  const person = people.find((person) => person.id === Number(id))
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
+
+router.delete('/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id))
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` })
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  )
+  return res.status(200).json({ success: true, data: newPeople })
+})
+
+
+router.route('/').get(getPeople).post(createPerson)
+router.route('/:id').get(getSinglePerson)
+module.exports  = router
