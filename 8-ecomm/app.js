fc469af3a4632463037d4345e977fe9d4807b310
Tornik2 <tornik2@yahoo.com>
2023-09-25 19:45:39 +0400

    add orderController + security

--- 8-ecomm/app.js
+++ 8-ecomm/app.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 require('express-async-errors')
 //rest of the packages
 const cookieParser = require('cookie-parser')
-const morgan = require('morgan')
 const fileUpload = require('express-fileupload')
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const mongoSanitize = require('express-mongo-sanitize')
 
 // express
 const express = require('express')
@@ -21,21 +25,31 @@ const authRouter = require('./routes/authRouter')
 const userRouter =  require('./routes/userRouter')
 const productRouter = require('./routes/productRouter')
 const reviewRouter = require('./routes/reviewRouter')
+const orderRouter = require('./routes/orderRouter')
 
-app.use(morgan('tiny'))
+// security
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(mongoSanitize())
+
+
 app.use(cookieParser(process.env.JWT_SECRET))
 app.use(express.json())
 app.use(fileUpload())
 
-app.get('/api/v1', (req, res)=> {
-    res.send('good togo')
-})
 
-app.use(express.static('/public'))
+app.use(express.static('./public'))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/reviews', reviewRouter)
+app.use('/api/v1/orders', orderRouter)
 
 
 app.use(notFound)
--- 8-ecomm/routes/userRouter.js
+++ 8-ecomm/routes/userRouter.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { getAllUsers, showCurrentUser, updateUser, updateUserPassword, getSingleUser } = require('../controllers/userController')
 const { authenticateUser, authorizeRoles } = require('../middleware/full-auth')
 
-router.route('/').get(authenticateUser, authorizeRoles('admin', 'ruler', 'boss'), getAllUsers)
+router.route('/').get([authenticateUser, authorizeRoles('admin', 'ruler', 'boss')], getAllUsers)
 
 router.route('/showMe').get( authenticateUser, showCurrentUser )
 router.route('/updateUser').patch(authenticateUser, updateUser)
